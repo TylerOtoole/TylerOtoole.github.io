4da0580fddf42aadf6c485e9597b578059b9ca3d
Ubuntu <ubuntu@ip-172-31-83-102.ec2.internal>
2020-03-06 18:15:00 +0000

    website

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -20,11 +20,44 @@ var level01 = function (window) {
                 { "type": "sawblade", "x": 600, "y": groundY - 110},
                 { "type": "sawblade", "x": 900, "y": groundY - 110},
                 { "type": "sawblade", "x": 1100, "y": groundY - 110},
-                { "type": "sawblade", "x": 1300, "y": groundY - 110},
+                { "type": "spikes", "x": 1300, "y": groundY - 15},
                 { "type": "spikes", "x": 1400, "y": groundY - 15},
-                { "type": "enemy", "x": 1200, "y": groundY - 100},
-                
-               
+                { "type": "enemy", "x": 600, "y": groundY - 100},
+                { "type": "sawblade", "x": 1700, "y": groundY - 110},
+                { "type": "spikes", "x": 2000, "y": groundY - 15},
+                { "type": "reward", "x": 2400, "y": groundY - 70},
+                { "type": "sawblade", "x": 2500, "y": groundY - 110},
+                { "type": "spikes", "x": 2700, "y": groundY - 15},
+                { "type": "spikes", "x": 3000, "y": groundY - 15},
+                { "type": "spikes", "x": 3100, "y": groundY - 15},
+                { "type": "spikes", "x": 3200, "y": groundY - 15},
+                { "type": "spikes", "x": 3300, "y": groundY - 15},
+                { "type": "spikes", "x": 3400, "y": groundY - 15},
+                { "type": "spikes", "x": 3500, "y": groundY - 15},
+                { "type": "spikes", "x": 3600, "y": groundY - 15},
+                { "type": "spikes", "x": 3700, "y": groundY - 15},
+                { "type": "spikes", "x": 3800, "y": groundY - 15},
+                { "type": "spikes", "x": 3900, "y": groundY - 15},
+                { "type": "spikes", "x": 4000, "y": groundY - 15},
+                { "type": "spikes", "x": 4100, "y": groundY - 15},
+                { "type": "spikes", "x": 4200, "y": groundY - 15},
+                { "type": "spikes", "x": 4300, "y": groundY - 15},
+                { "type": "spikes", "x": 4400, "y": groundY - 15},
+                { "type": "spikes", "x": 4500, "y": groundY - 15},
+                { "type": "spikes", "x": 4600, "y": groundY - 15},
+                { "type": "spikes", "x": 4700, "y": groundY - 15},
+                { "type": "spikes", "x": 4800, "y": groundY - 15},
+                { "type": "reward", "x": 4900, "y": groundY - 50},
+                { "type": "reward", "x": 5000, "y": groundY - 50},
+                { "type": "reward", "x": 5100, "y": groundY - 50},
+                { "type": "reward", "x": 5500, "y": groundY - 50},
+                { "type": "reward", "x": 5700, "y": groundY - 50},
+                { "type": "reward", "x": 5900, "y": groundY - 50},
+                { "type": "reward", "x": 6100, "y": groundY - 50},
+                { "type": "reward", "x": 6300, "y": groundY - 50},
+                { "type": "reward", "x": 6500, "y": groundY - 50},
+                { "type": "reward", "x": 6700, "y": groundY - 50},
+            
             ]
         };
         window.levelData = levelData;
@@ -36,35 +69,43 @@ var level01 = function (window) {
        
         function createSawBlade(x , y){
         
-        var hitZoneSize = 25;
-        var damageFromObstacle = 10;
-        var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
-            sawBladeHitZone.x = x;
-            sawBladeHitZone.y = y;
-                game.addGameItem(sawBladeHitZone);
-                
-        var obstacleImage = draw.bitmap('img/sawblade.png');
-            sawBladeHitZone.addChild(obstacleImage);
-            obstacleImage.x = -25;
-            obstacleImage.y = -25;
-        
+            var hitZoneSize = 25;
+            var damageFromObstacle = 10;
+            var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+                sawBladeHitZone.x = x;
+                sawBladeHitZone.y = y;
+                    game.addGameItem(sawBladeHitZone);
+                    
+            var obstacleImage = draw.bitmap('img/sawblade.png');
+                sawBladeHitZone.addChild(obstacleImage);
+                obstacleImage.x = -25;
+                obstacleImage.y = -25;
         
         }
         
         for (var key = 0;key <levelData.gameItems.length;key++){
             var gameItemObject = levelData.gameItems[key];
+            
             if(gameItemObject.type === 'sawblade'){
                 createSawBlade(gameItemObject.x, gameItemObject.y);
             }
-        } 
+            
             if(gameItemObject.type === 'spikes'){
                 createSpikes(gameItemObject.x, gameItemObject.y);
-            
-            
             }
+            
              if(gameItemObject.type === 'enemy'){
                 createEnemy(gameItemObject.x, gameItemObject.y);
              }
+             
+              if(gameItemObject.type === 'reward'){
+                createReward(gameItemObject.x, gameItemObject.y);
+              }
+              if(gameItemObject.type === 'reward2'){
+                createReward(gameItemObject.x, gameItemObject.y);
+              }
+        } 
+            
              
              
              
@@ -72,26 +113,24 @@ var level01 = function (window) {
              
         function createSpikes(x,y) {
             
-     var hitZoneSize = 25;
-    
-     var damageFromObstacle = 10;
-    
-     var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
-        sawBladeHitZone.x = x;
-        sawBladeHitZone.y = y;
-           
-            game.addGameItem(sawBladeHitZone);
-    
-     var obstacleImage = draw.bitmap('img/spikes.png');
-         sawBladeHitZone.addChild(obstacleImage);
-         obstacleImage.x = -50;
-         obstacleImage.y = -25;
-         obstacleImage.scaleX = .1;
-         obstacleImage.scaleY = .1;
-
-            
-            
-    }
+         var hitZoneSize = 25;
+        
+         var damageFromObstacle = 10;
+        
+         var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
+            sawBladeHitZone.x = x;
+            sawBladeHitZone.y = y;
+               
+                game.addGameItem(sawBladeHitZone);
+        
+         var obstacleImage = draw.bitmap('img/spikes.png');
+             sawBladeHitZone.addChild(obstacleImage);
+             obstacleImage.x = -50;
+             obstacleImage.y = -25;
+             obstacleImage.scaleX = .1;
+             obstacleImage.scaleY = .1;
+         
+        }
     
     
     function createEnemy(x,y){
@@ -118,12 +157,47 @@ var level01 = function (window) {
         }
     }
     
-    
+    function createReward(x , y){
 
-        
-        
-        
-        
+        var reward = game.createGameItem('reward', 50);
+            reward.x = x;
+            reward.y = y;
+            reward.scaleX = .5
+            reward.scaleY = .5
+            reward.velocityX = -2;
+            
+            var picture = draw.bitmap('img/iceicebaby.png');
+            picture.x = -80;
+            picture.y = -100;
+            
+            reward.addChild(picture);
+            game.addGameItem(reward);
+            
+            reward.onPlayerCollision = function() {
+                game.increaseScore(1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000);
+                reward.fadeOut();
+            };
+    }    
+    
+        function createReward2(x , y){
+
+        var reward2 = game.createGameItem('reward2', 25);
+            reward2.x = x;
+            reward2.y = y;
+            reward2.velocityX = -2;
+            
+            var picture = draw.bitmap('img/Water.png');
+            picture.x = -25;
+            picture.y = -25;
+            
+            reward2.addChild(picture);
+            game.addGameItem(reward2);
+            
+            reward2.onPlayerCollision = function() {
+                game.increaseScore(50);
+                reward2.fadeOut();
+            };
+    }    
         
         // DO NOT EDIT CODE BELOW HERE
     };
